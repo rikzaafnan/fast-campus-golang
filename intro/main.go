c1c1616f5c240667bf40d7a02e64f97551d46ad0
rikzaafnan <rikzaafnan@gmail.com>
2024-08-31 11:17:16 +0700

    add intro

--- intro/main.go
+++ intro/main.go
@@ -2,8 +2,34 @@ package main
 
 import "fmt"
 
+func init() {
+	fmt.Println("Hello from init 2")
+}
+
 func main() {
 
 	fmt.Println(true)
 
+	// iteration
+	var colors = [...]string{"merah", "kuning", "hijau"}
+	fmt.Println(len(colors))
+	fmt.Println(colors)
+
+	var i = 0
+	for {
+		if i%2 != 0 {
+			i++
+			continue
+		} else if i == 10 {
+			break
+		}
+
+		fmt.Println("Nilai : ", i)
+		i++
+	}
+
+}
+
+func init() {
+	fmt.Println("Hello from init 1")
 }
